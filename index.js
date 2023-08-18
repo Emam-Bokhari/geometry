0cd572b12e431fa5c159ecf1bd974e788df80431
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-08-18 09:26:42 +0600

    Parallelogram & rectangle done

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ function valuePlace(element,inner){
     calledElement.innerText=inner
 }
 
-// EventListener
+// Event Listener Triangle
 document.querySelector('#triangle-btn').addEventListener('click', function(){
 const firstInputValue=calledInputFeildById('first-input');
 const secondInputValue=calledInputFeildById('second-input');
 const triangleAreaCalculate= 0.5 * firstInputValue * secondInputValue;
 valuePlace('value-inner', triangleAreaCalculate)
+})
+
+// Event Listener Rectangle
+document.querySelector('#rectangle-btn').addEventListener('click', function(){
+const firstInputValue = calledInputFeildById('rectangle-first-input');
+const secondInputValue = calledInputFeildById('rectangle-second-input');
+const rectangleAreaCAlculate = firstInputValue * secondInputValue;
+valuePlace('rectangle-value-inner', rectangleAreaCAlculate);
+})
+
+// Evenet Listener parallelogram
+document.querySelector('#parallelogram-btn').addEventListener('click', function(){
+    const firstInputValue = calledInputFeildById('parallelogram-first-input');
+    const secondInputValue=calledInputFeildById('parallelogram-second-input');
+    const parallelogramAreaCalculate = firstInputValue * secondInputValue; 
+    valuePlace('parallelogram-value-inner', parallelogramAreaCalculate);
 })
