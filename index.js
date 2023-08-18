b7154faca6903e98b1c2272a2baee4ef41e2ddb1
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-08-18 11:38:44 +0600

    Number validation done

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function valuePlace(element, inner) {
 document.querySelector('#triangle-btn').addEventListener('click', function () {
     const firstInputValue = calledInputFeildById('first-input');
     const secondInputValue = calledInputFeildById('second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+       return alert('Please insert a number');
+    }
     const triangleAreaCalculate = 0.5 * firstInputValue * secondInputValue;
     const twoDecimal=triangleAreaCalculate.toFixed(2);
     valuePlace('value-inner', twoDecimal)
@@ -25,6 +28,9 @@ document.querySelector('#triangle-btn').addEventListener('click', function () {
 document.querySelector('#rectangle-btn').addEventListener('click', function () {
     const firstInputValue = calledInputFeildById('rectangle-first-input');
     const secondInputValue = calledInputFeildById('rectangle-second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+        return alert('Please insert a number');
+     }
     const rectangleAreaCAlculate = firstInputValue * secondInputValue;
     const twoDecimal=rectangleAreaCAlculate.toFixed(2);
     valuePlace('rectangle-value-inner', twoDecimal);
@@ -34,6 +40,9 @@ document.querySelector('#rectangle-btn').addEventListener('click', function () {
 document.querySelector('#parallelogram-btn').addEventListener('click', function () {
     const firstInputValue = calledInputFeildById('parallelogram-first-input');
     const secondInputValue = calledInputFeildById('parallelogram-second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+        return alert('Please insert a number');
+     }
     const parallelogramAreaCalculate = firstInputValue * secondInputValue;
     const twoDecimal=parallelogramAreaCalculate.toFixed(2);
     valuePlace('parallelogram-value-inner', twoDecimal);
@@ -42,6 +51,9 @@ document.querySelector('#parallelogram-btn').addEventListener('click', function 
 document.querySelector('#rhombus-btn').addEventListener('click', function () {
     const firstInputValue = calledInputFeildById('rhombus-first-input');
     const secondInputValue = calledInputFeildById('rhombus-second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+        return alert('Please insert a number');
+     }
     const rhombusAreaCalculate = 0.5 * firstInputValue * secondInputValue;
     const twoDecimal=rhombusAreaCalculate.toFixed(2);
     valuePlace('rhombus-value-inner', twoDecimal);
@@ -50,6 +62,9 @@ document.querySelector('#rhombus-btn').addEventListener('click', function () {
 document.querySelector('#pentagon-btn').addEventListener('click', function(){
     const firstInputValue = calledInputFeildById('pentagon-first-input');
     const secondInputValue = calledInputFeildById('pentagon-second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+        return alert('Please insert a number');
+     }
     const pentagonAreaCalculate= 0.5 * firstInputValue * secondInputValue;
     const twoDecimal=pentagonAreaCalculate.toFixed(2);
     valuePlace('pentagon-value-inner', twoDecimal);
@@ -58,6 +73,9 @@ document.querySelector('#pentagon-btn').addEventListener('click', function(){
 document.querySelector('#ellipse-btn').addEventListener('click', function(){
     const firstInputValue = calledInputFeildById('ellipse-first-input');
     const secondInputValue = calledInputFeildById('ellipse-second-input');
+    if(isNaN(firstInputValue) || isNaN(secondInputValue)){
+        return alert('Please insert a number');
+     }
     const ellipseAreaCalculate = 3.1416 * firstInputValue * secondInputValue;
     const twoDecimal=ellipseAreaCalculate.toFixed(2);
     valuePlace('ellipse-value-inner', twoDecimal);
